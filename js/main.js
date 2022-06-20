f9ee29a703e9c73df4b30ae532994e30a2fe9f3f
nezaga <nicolas.zaga@gmail.com>
2022-06-20 18:01:09 -0300

    algoritmo utilzando un for para sumar números ingresados por el usuario

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let vueltas = parseInt(prompt("¿Cuántas vueltas querés dar?"));
+let numero = parseInt(prompt("¿Qué número quéres sumar?"));
+alert("OK, ahí vamos! Sumaremos " + numero + ", " + vueltas + " veces");
+let suma = numero;
+
+for(let i = 1; i <= vueltas; i++) {
+    suma = suma + numero;
+    console.log(suma);
+}
+
+alert("Luego de " + vueltas + " vuelta(s), el resultado es: " + suma);
